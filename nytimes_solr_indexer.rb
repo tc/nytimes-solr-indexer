15960b6a41e106b08f0679893e47bfb8281a4cc3
Tommy Chheng <tommy.chheng@gmail.com>
2010-04-08 16:54:21 -0700

    default to using rsolr-direct

--- nytimes_solr_indexer.rb
+++ nytimes_solr_indexer.rb
@@ -1,26 +1,40 @@
 require 'java'
 require 'rubygems'
 require 'rsolr'
+require 'rsolr-direct'
 require 'timestools.jar'
 
 class String
+  def starts_with?(prefix)
+    prefix = prefix.to_s
+    self[0, prefix.length] == prefix
+  end
+ 
   def ends_with?(suffix)
-  suffix = suffix.to_s
-  self[-suffix.length, suffix.length] == suffix      
+    suffix = suffix.to_s
+    self[-suffix.length, suffix.length] == suffix      
   end
 end
 
 def untar_dir(dir)
   tar_files = Dir[File.join(dir, "**", "*")].map{|f| f if File.basename(f).ends_with?(".tgz")}.compact
-
+  
   tar_files.each do |file|
     puts "Untaring #{file}"
     system("tar -C #{File.dirname(file)} -zxvf #{file}")
   end  
 end
 
-def index_dir(dir, solr_server_url="http://localhost:8983/solr")
-  solr = RSolr.connect :url=>solr_server_url
+def index_dir(dir, opts={})
+  solr = nil
+  if opts[:solr_home] != nil
+    RSolr.load_java_libs
+    solr = RSolr.connect :direct, :solr_home => opts[:solr_home]
+  elsif opts[:solr_server_url] != nil
+    solr = RSolr.connect :url=> opts[:solr_server_url]
+  else
+    raise "You must specify a solr_home or solr_server_url"
+  end
   solr.delete_by_query "*:*"
 
   docs = []
@@ -114,14 +128,24 @@ end
 puts "Indexing NYTimes Corpus"
 puts "-----"
 
-if(ARGV.size == 0)
-  puts " This script will untar the xml files and POST them to Solr"
-  puts " Specify a path the nytimes corpus data folder"
-  puts "jruby nytimes_solr_indexer.rb \"path/to/nytimes data/data\""
+if(ARGV.size != 2)
+  puts " This script will untar the xml files and index them with Solr"
+  puts " Specify a path the nytimes corpus data folder and solr home"
+  puts "jruby nytimes_solr_indexer.rb \"path/to/nytimes data/data\" \"/path/to/solr/home\""
+  puts " You can also post a solr server by specifying a solr server in the second parameter"
+  puts "jruby nytimes_solr_indexer.rb \"path/to/nytimes data/data\" \"http://localhost:8983/solr\""
 else
   dir = ARGV[0] #"/Users/wheels/data/tmp_data"
+  server_or_solr_dir = ARGV[1] #"/Users/wheels/src/java/nytimes/solr/solr" or "http://localhost:8983/solr"
+  opts= {}
+  if server_or_solr_dir.starts_with?("http://")
+    opts[:solr_server_url] = server_or_solr_dir
+  else
+    opts[:solr_home] = server_or_solr_dir
+  end
+      
   puts "Processing #{ARGV[0]}"
-  untar_dir(dir)
-  index_dir(dir)  
+  #untar_dir(dir)
+  index_dir(dir, opts)  
   puts "Done!"
 end
